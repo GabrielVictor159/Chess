536dac23889d3e682269eae82df0b024e93d70a8
GabrielVictor159 <gabrielvictor159487@gmail.com>
2022-11-18 02:52:52 -0300

    consertando bugs da IA

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import Chessboard from './componentes/Chessboard';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Chessboard dificulty='easy'/>
+      <Chessboard dificulty='hard'/>
     </View>
   );
 }
